b5fea6e9d15b9b00f41aa0c024b4205bdf3a08f1
jnarang <jatin.narang@hashedin.com>
2019-02-13 14:56:00 +0530

    Code review

---
+++ client/src/components/Card/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './styles.scss';
+import PropTypes from 'prop-types';
+
+class Card extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		const { answerClick, option, selectedCard } = this.props;
+		return (
+			<button
+				className={`card-button card-button-${answerClick} card-button-${selectedCard} color-change-${selectedCard} ${this
+					.props.color}`}
+				type="button"
+				onClick={this.props.handleClick}
+			>
+				{option}
+			</button>
+		);
+	}
+}
+
+Card.prototypes = {
+	answerClick: PropTypes.bool,
+	option: PropTypes.string,
+	selectedCard: PropTypes.bool
+};
+
+export default Card;
